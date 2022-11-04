07a601f34c980a5399fede36ffe25ddcbf176a60
Zafer Akçalı <zaferakcali@yahoo.com>
2022-11-04 11:30:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,6 +16,6 @@ echo ('sum of times cited='.$citations['timesCited'].'<br/>');
 echo ('publications in wos='.$citations['numPublicationsInWosCc'].'<br/>');
 foreach( array_reverse ($citations['citationsPerYear'], true) as $year=>$citation) {
 	if($citation !== '0' )
-		echo ($citation.' citations in year '.$year.'<br/>');
+		echo ($citation.' citation(s) in year '.$year.'<br/>');
     }
 ?>
